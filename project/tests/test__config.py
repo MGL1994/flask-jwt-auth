3f1ce77299a7d2b1dc752be9b803e941bcc645a8
MGL1994 <michaelgl1994@gmail.com>
2020-01-22 17:13:09 +0000

    test project complete

--- project/tests/test__config.py
+++ project/tests/test__config.py
@@ -15,8 +15,12 @@ class TestDevelopmentConfig(TestCase):
         return app
 
     def test_app_is_development(self):
+        self.assertFalse(app.config['SECRET_KEY'] == 'my_precious')
         self.assertTrue(app.config['DEBUG'] is True)
         self.assertFalse(current_app is None)
+        self.assertTrue(
+            app.config['SQLALCHEMY_DATABASE_URI'] == 'postgresql://postgres:@localhost/flask_jwt_auth'
+        )
 
 
 class TestTestingConfig(TestCase):
@@ -26,6 +30,9 @@ class TestTestingConfig(TestCase):
 
     def test_app_is_testing(self):
         self.assertTrue(app.config['DEBUG'])
+        self.assertTrue(
+            app.config['SQLALCHEMY_DATABASE_URI'] == 'postgresql://postgres:@localhost/flask_jwt_auth_test'
+        )
 
 
 class TestProductionConfig(TestCase):
